f363c380c262e617347da24cfbdc84465a65101b
vitoraoki <vitorkaoki@gmail.com>
2022-12-28 01:19:03 -0300

    Create buildSrc to manage dependencies

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,20 @@
+object Dependencies {
+    const val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+    const val daggerCompiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+
+    const val activityKtx = "androidx.activity:activity-ktx:${Versions.activity}"
+    const val activityCompose = "androidx.activity:activity-compose:${Versions.activity}"
+
+    const val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle}"
+    const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+
+    const val composeUi = "androidx.compose.ui:ui:${Versions.compose}"
+    const val composePreview = "androidx.compose.ui:ui-tooling-preview:${Versions.compose}"
+    const val composeMaterial = "androidx.compose.material:material:${Versions.composeMaterial}"
+    const val composeUiTooling = "androidx.compose.ui:ui-tooling:${Versions.compose}"
+    const val composeUiTestManifest = "androidx.compose.ui:ui-test-manifest:${Versions.compose}"
+
+    const val androidxCoreKtx = "androidx.core:core-ktx:${Versions.androidCoreKtx}"
+
+    const val junit = "junit:junit:${Versions.junit}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+    const val dagger = "2.44.2"
+    const val activity = "1.6.1"
+    const val lifecycle = "2.5.1"
+    const val compose = "1.3.2"
+    const val composeMaterial = "1.3.1"
+    const val androidCoreKtx = "1.9.0"
+    const val junit = "4.13.2"
+}
